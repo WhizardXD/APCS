d226387c4bbe6e8369b5af8fa7e3f47fe699324d
WhizardXD <33294017+WhizardXD@users.noreply.github.com>
2018-09-25 01:27:35 -0700

    gitignored build batch file

--- Processing/Lightning/buildfilelightning.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-title lightningbuild
-echo Opening file...
-C:\"Program Files"\Processing-3.4\processing-java.exe --force --sketch=C:/Users/"Chris Y"/Documents/GitHub/APCS/Processing/Lightning --output=C:/Users/"Chris Y"/Documents/GitHub/APCS/Processing/Lightning/out --run --force --sketch=C:/Users/"Chris Y"/Documents/GitHub/APCS/Processing/Lightning --output=C:/Users/"Chris Y"/Documents/GitHub/APCS/Processing/Lightning\out --run
-
-:End
-cmd /k
