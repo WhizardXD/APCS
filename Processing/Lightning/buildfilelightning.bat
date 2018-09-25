dd59bf6de225519b78dbc5158d394746c2d34ed4
WhizardXD <33294017+WhizardXD@users.noreply.github.com>
2018-09-25 01:23:57 -0700

    basic lightning program working

--- Processing/Lightning/buildfilelightning.bat
+++ Processing/Lightning/buildfilelightning.bat
@@ -1,7 +1,7 @@
 @echo off
 title lightningbuild
 echo Opening file...
-processing-java --force --sketch=C:/Users/chyuan1/Documents/GitHub/APCS/Processing/Lightning --output=C:/Users/chyuan1/Documents/GitHub/APCS/Processing/Lightning/out --run --force --sketch=C:/Users/chyuan1/Documents/GitHub/APCS/Processing/Lightning --output=C:/Users/chyuan1/Documents/GitHub/APCS/Processing/Lightning\out --run
+C:\"Program Files"\Processing-3.4\processing-java.exe --force --sketch=C:/Users/"Chris Y"/Documents/GitHub/APCS/Processing/Lightning --output=C:/Users/"Chris Y"/Documents/GitHub/APCS/Processing/Lightning/out --run --force --sketch=C:/Users/"Chris Y"/Documents/GitHub/APCS/Processing/Lightning --output=C:/Users/"Chris Y"/Documents/GitHub/APCS/Processing/Lightning\out --run
 
 :End
 cmd /k
