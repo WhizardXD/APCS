a86749f36cee88cd3785febab92722abd757de40
WhizardXD <33294017+WhizardXD@users.noreply.github.com>
2018-09-26 14:37:42 -0700

    fixed gitignore

--- Processing/Dice/buildfiledice.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-title lightningbuild
-echo Opening file...
-processing-java --force --sketch=C:/Users/chyuan1/Documents/GitHub/APCS/Processing/Dice --output=C:/Users/chyuan1/Documents/GitHub/APCS/Processing/Dice/out --run --force --sketch=C:/Users/chyuan1/Documents/GitHub/APCS/Processing/Dice --output=C:/Users/chyuan1/Documents/GitHub/APCS/Processing/Dice\out --run
-
-:End
-cmd /k
