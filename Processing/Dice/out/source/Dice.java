5b3b181276425aa4ddc48c76b2778f559f77a03a
WhizardXD <33294017+WhizardXD@users.noreply.github.com>
2018-09-27 14:39:22 -0700

    finished Dice basic function/look

--- Processing/Dice/out/source/Dice.java
+++ Processing/Dice/out/source/Dice.java
@@ -21,6 +21,9 @@ public void setup() {
 
 public void draw() {
     background(197);
+
+	Die bob1 = new Die(mouseX,mouseY);
+	bob1.show();
 }
 
 public void mousePressed() {
@@ -30,19 +33,82 @@ public void mousePressed() {
 
 class Die {//models one single dice cube
 
-	//variable declarations here
+	int myX, myY; 
+	int rollNum = (int)(Math.random()*6)+1;
+	int rotAte = (int)(Math.random()*2)+1;
 	
 	Die(int x, int y) {
         //constructor
-		//variable initializations here
+		myX = x;
+		myY = y;
 	}
 
 	public void roll() {
-		//your code here
+		rollNum = (int)(Math.random()*6)+1;
 	}
 
+
 	public void show() {
-		//your code here
+		fill(255, 255, 255);
+		rect(myX - 24, myY - 24, 48, 48);
+
+		if (rollNum == 1) {
+			fill(0, 0, 0);
+			ellipse(myX, myY, 6, 6);
+		} else if (rollNum == 2) {
+			fill(0, 0, 0);
+			ellipse(myX - 10, myY - 10, 6, 6);
+			ellipse(myX + 10, myY + 10, 6, 6);
+		} else if (rollNum == 3) {
+			fill(0, 0, 0);
+			ellipse(myX, myY, 6, 6);
+			ellipse(myX - 12, myY - 12, 6, 6);
+			ellipse(myX + 12, myY + 12, 6, 6);		
+		} else if (rollNum == 4) {
+			fill(0, 0, 0);
+			ellipse(myX - 10, myY - 10, 6, 6);
+			ellipse(myX + 10, myY + 10, 6, 6);
+			ellipse(myX - 10, myY + 10, 6, 6);
+			ellipse(myX + 10, myY - 10, 6, 6);
+		} else if (rollNum == 5) {
+			fill(0, 0, 0);
+			ellipse(myX, myY, 6, 6);
+			ellipse(myX - 12, myY - 12, 6, 6);
+			ellipse(myX + 12, myY + 12, 6, 6);
+			ellipse(myX - 12, myY + 12, 6, 6);
+			ellipse(myX + 12, myY - 12, 6, 6);
+		} else if (rollNum == 6) {
+			rotAte = (int)(Math.random()*2)+1;
+			if (rotAte == 1) {
+				fill(0, 0, 0);
+				ellipse(myX - 11, myY - 11, 6, 6);
+				ellipse(myX + 11, myY + 11, 6, 6);
+				ellipse(myX - 11, myY + 11, 6, 6);
+				ellipse(myX + 11, myY - 11, 6, 6);
+				ellipse(myX + 11, myY, 6, 6);
+				ellipse(myX - 11, myY, 6, 6);
+			} else {
+				fill(0, 0, 0);
+				ellipse(myX - 11, myY - 11, 6, 6);
+				ellipse(myX + 11, myY + 11, 6, 6);
+				ellipse(myX - 11, myY + 11, 6, 6);
+				ellipse(myX + 11, myY - 11, 6, 6);
+				ellipse(myX, myY + 11, 6, 6);
+				ellipse(myX, myY - 11, 6, 6);
+
+			}
+		}
+
+
+
+
+
+
+
+
+
+
+
 	}
 }
   public void settings() {  size(500, 500); }
